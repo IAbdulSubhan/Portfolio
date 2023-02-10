c11a8f116a91c8db7df6d23582602592ea26d7b2
ABDUL SUBHAN <p200086@pwr.nu.edu.pk>
2023-02-11 00:27:29 +0500

    Add files via upload

---
+++ Basics_of_Dart.dart
@@ -0,0 +1,65 @@
+void main() {
+  print("Hello Subhan");
+// //  ========== var varibale decide run data type of variable
+  
+//   var v1 = "Mian Abdul Subhan";
+  
+//   var v2 = 40;
+  
+//   print ("About Var datatype of varible: \n");
+//   print(v1);
+//   print(v2);
+//   
+// =========================
+  
+// //   =========="Const" unchange able data from the varibles means it store constant data
+// // it also decide type of data at run time
+//   const c = 30;
+  
+// //   const c = 30.9;     this line creating error due to overwriting the variable
+  
+//   const b = "Abdul Subhan";
+  
+//   print(c);
+//   print(b);
+  
+// ===================type casting ===============================
+
+//   ==========string to integer
+  
+//   var one = int .parse("1");
+// //   assert is same like if condtion means it also chechk that the thing is same whuch we provide as input or not
+//   assert(one == 1);
+  
+//   one += 10;
+  
+//   print(one);
+  
+//   
+//   ==========integer to string
+  
+//   var one =  1.toString();
+  
+//   one = one + (" it is converted into string");
+  
+//   print(one);
+  
+  
+// ============double/ float to string
+  
+  //decimal to string
+  
+//   var one = 3.1234.toString();
+//   var oneD = 3.1234.toStringAsFixed (2);
+//   assert (oneD == '3.12');
+//   print (oneD);
+  
+// ============while loop
+  int num = 0;
+  while(num <10) {
+  if (num % 2 == 0)
+  print (num);
+  num++;
+  }
+
+}
